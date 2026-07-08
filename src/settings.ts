4e776247aa293e862134b0ba48633b9091d128c1
Stephen Santilli <stephen.j.santilli@gmail.com>
2026-07-08 12:42:09 -0400

    Updated repo name and build.yml name

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'PiTherm';
 /**
  * This must match the name of your plugin as defined the package.json `name` property
  */
-export const PLUGIN_NAME = 'homebridge-pitherm';
+export const PLUGIN_NAME = 'homebridge-pi-therm';
